77d49f618d4fcb80dd25acf6639b99c534607b5a
Marie Malarme <malarmemarie@gmail.com>
2021-02-06 18:19:18 +0000

    Set up the Faturas page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from './lib.js'
+import { faturas } from './faturas.js'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+    <Page>
+      <Header>
+        <Title>Faturas</Title>
+        <Link
+          href="https://faturas.portaldasfinancas.gov.pt/consultarDocumentosAdquirente.action"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Portal das Finanças
+        </Link>
+      </Header>
+      {categories.map((category) => (
+        <Faturas faturas={faturas} category={category} />
+      ))}
+    </Page>
+  )
 }
 
-export default App;
+const Faturas = ({ faturas, category }) => {
+  const registered_category = category === 'registered'
+  const filtered_faturas = faturas.filter(
+    ({ registered, status }) =>
+      (registered_category && registered) ||
+      (!registered && status === category),
+  )
+
+  return (
+    <Table>
+      <Section
+        grey3={registered_category}
+        b_grey2={registered_category}
+        sapphire4={!registered_category}
+        b_sapphire2={!registered_category}
+      >
+        {category}
+      </Section>
+      {filtered_faturas.map((fatura) => (
+        <Fatura fatura={fatura} />
+      ))}
+    </Table>
+  )
+}
+
+const Fatura = ({ fatura }) => {
+  const { name, seller, price, date, category, scope, registered } = fatura
+  return (
+    <Row>
+      <Column>{name}</Column>
+      <Column>{seller}</Column>
+      <Column>{price}</Column>
+      <Column>{date}</Column>
+      <Column>{category}</Column>
+      <Column>{scope}</Column>
+      <Button ba={registered} shadow_a_s={!registered}>
+        {registered && 'un'}register
+      </Button>
+    </Row>
+  )
+}
+
+const Page = Component.pa100.div()
+const Header = Component.flex.jc_between.ai_baseline.div()
+const Title = Component.fs60.grey6.mono.div()
+const Link = Component.text_dec_none.grey6.bb.b_grey2.a()
+const Table = Component.mt100.div()
+const Section = Component.fs18.mb25.pv10.uppercase.mono.ls2.bb.div()
+const Row = Component.flex.ai_center.bb.b_grey2.pv20.div()
+const Column = Component.flex1.div()
+const Button = Component.w110.text_center.b_rad4.c_pointer.pv5.ph15.uppercase.fs10.ls2.b_grey2.div()
+
+const categories = ['pending', 'unregistered', 'registered']
+
+export default App
---
+++ src/lib.js
@@ -0,0 +1,5 @@
+import { atomizify, flagify } from 'atomizify'
+
+atomizify({ custom_classes: { mono: `font-family: 'IBM Plex Mono'` } })
+
+export const { Component } = flagify()
